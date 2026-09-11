3a1b1613c5fb82f8ea101fba6188d79cb269f600
JOAQUIN FUENZALIDA <jfuenzalidae@uc.cl>
2026-09-10 22:10:42 -0300

    Add second level with collectibles and scoring

--- scripts/ui/level_selector.gd
+++ scripts/ui/level_selector.gd
@@ -6,14 +6,19 @@ extends Control
 
 const LEVELS := [
 	"res://scenes/main.tscn",
+	"res://scenes/level_2.tscn",
 ]
 
 func _ready() -> void:
 	$BackButton.pressed.connect(_on_back_pressed)
 	$Center/LevelsRow/Level1Button.pressed.connect(_on_level_1_pressed)
+	$Center/LevelsRow/Level2Button.pressed.connect(_on_level_2_pressed)
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
 
 func _on_level_1_pressed() -> void:
 	get_tree().change_scene_to_file(LEVELS[0])
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file(LEVELS[1])
